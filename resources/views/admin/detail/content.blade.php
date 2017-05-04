f782598e0ca32dc8da4e65fa4a815ad344db4b72
quocthan <daothan12111@gmail.com>
2017-05-04 23:25:50 +0700

    --user=root

--- resources/views/admin/detail/content.blade.php
+++ resources/views/admin/detail/content.blade.php
@@ -2,26 +2,20 @@
 
 @section('content')
 	<div class="container">
-		<div class="panel">
-			<div class="panel-body">
-				<div class="col-md-8 col-md-offset-2">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2 col-centered">
 					@foreach($content as $data)
 						<div class="panel-tittle">
-							<div class="col-md-offset-3">
-								<h2><strong>{!!htmlspecialchars_decode($data["tittle"])!!}</strong></h2>
-							</div>
+							<h2><strong>{!!htmlspecialchars_decode($data["tittle"])!!}</strong></h2>
 						</div>
 						<div class="panel-heading">
-							<div class="col-md-offset-3">
-								<img src="{{asset('storage/uploads/detail_images/'.$data["tittle"].'/'.$data["images"])}}">
-							</div>
+							<img src="{{asset('storage/uploads/detail_images/'.$data["tittle"].'/'.$data["images"])}}">
 							{!!htmlspecialchars_decode($data["introduce"])!!}
 						</div>
 						<div class="panel-body">
 							{!!htmlspecialchars_decode($data["content"]) !!}
 						</div>
 					@endforeach
-				</div>
 			</div>
 			<a href="{{route('admin.detail.show')}}"><button class="btn btn-success pull-right">Show details</button></a>
 		</div>
--- resources/views/admin/detail/show.blade.php
+++ resources/views/admin/detail/show.blade.php
@@ -3,88 +3,88 @@
 @section('content')
 
 	<div class="container">
-		<div class="panel">
-			<div class="panel">
-				<div class="col-md-offset-5">
-					<h2><strong>Show details</strong></h2>
+		<div class="row">
+			<div class="col-md-offset-0 col-centered">
+				<div class="panel">
+					<div class="col-md-offset-5">
+						<h2><strong>Show details</strong></h2>
+					</div>
+				</div>
+
+				<div class="panel-body">
+					<table class="table table-striped table-bordered table-hover">
+						<thead>
+							<tr align="center">
+								<th>No</th>
+								<th>Tittle</th>
+								<th>Parent category</th>
+								<th>Data</th>
+								<th>User</th>
+								<th>View Content</th>
+								<th>Edit</th>
+								<th>Delete</th>
+							</tr>
+						</thead>
+
+						<tbody>
+							<?php $no=0;?>
+							@foreach($detail as $data)
+							<?php $no++;?>
+
+							<tr class="odd gradeX" align="center">
+								<th>{{$no}}</th>
+								<td>{{$data->tittle}}</td>
+
+								<!-- Show parent category -->
+								<td>
+								<?php $parent = DB::table('categories')->where('id', $data["cate_id"])->first();
+								echo $parent->name;
+								?>
+
+								<!-- Show User -->
+								</td>
+								<!-- Show Date -->
+								<td>
+									<?php
+										echo \Carbon\Carbon::createFromTimestamp(strtotime($data["created_at"]))->diffForHumans();
+									?>
+								</td>
+								<!-- Show User -->
+								<td>
+									<?php
+										$user = DB::table('users')->where('id', $data["user_id"])->first();
+										echo $user->email;
+									?>
+								</td>
+								<!-- Show Content -->
+								<td>
+									<a href="{{route('admin.detail.content', $data->id)}}">
+										<button class="btn btn-link"><span class="glyphicon glyphicon-list-alt">&nbsp</span>View Content</button>
+									</a>
+								</td>
+								<!-- Edit -->
+								<td>
+									<a href="{{route('admin.detail.getEdit', $data->id)}}">
+										<button class="btn btn-link"><span class="glyphicon glyphicon-pencil">&nbsp</span>Edit</button>
+									</a>
+								</td>
+								<!-- Delete -->
+								<td>
+									<form action="{{route('admin.detail.delete',$data->id)}}" method="POST" role="form">
+										{{csrf_field()}}
+										<input type="hidden" name="method" value="DELETE">
+										<input type="hidden" name="id" value="{{$data->id}}">
+										<button class="btn btn-link" id="delete" type="submit" onclick="return confirmdelete('Are you sure you want to delete -{{$data->tittle}}- ?')"><span class="glyphicon glyphicon-trash">&nbsp</span>Delete</button>
+									</form>
+								</td>
+							</tr>
+							@endforeach
+						</tbody>
+					</table>
 				</div>
 			</div>
-
-			<div class="panel-body">
-				<table class="table table-striped table-bordered table-hover">
-					<thead>
-						<tr align="center">
-							<th>No</th>
-							<th>Tittle</th>
-							<th>Parent category</th>
-							<th>Data</th>
-							<th>User</th>
-							<th>View Content</th>
-							<th>Edit</th>
-							<th>Delete</th>
-						</tr>
-					</thead>
-
-					<tbody>
-						<?php $no=0;?>
-						@foreach($detail as $data)
-						<?php $no++;?>
-
-						<tr class="odd gradeX" align="center">
-							<th>{{$no}}</th>
-							<td>{{$data->tittle}}</td>
-
-							<!-- Show parent category -->
-							<td>
-							<?php $parent = DB::table('categories')->where('id', $data["cate_id"])->first();
-							echo $parent->name;
-							?>
-
-							<!-- Show User -->
-							</td>
-							<!-- Show Date -->
-							<td>
-								<?php
-									echo \Carbon\Carbon::createFromTimestamp(strtotime($data["created_at"]))->diffForHumans();
-								?>
-							</td>
-							<!-- Show User -->
-							<td>
-								<?php
-									$user = DB::table('users')->where('id', $data["user_id"])->first();
-									echo $user->email;
-								?>
-							</td>
-							<!-- Show Content -->
-							<td>
-								<a href="{{route('admin.detail.content', $data->id)}}">
-									<button class="btn btn-link"><span class="glyphicon glyphicon-list-alt">&nbsp</span>View Content</button>
-								</a>
-							</td>
-							<!-- Edit -->
-							<td>
-								<a href="{{route('admin.detail.getEdit', $data->id)}}">
-									<button class="btn btn-link"><span class="glyphicon glyphicon-pencil">&nbsp</span>Edit</button>
-								</a>
-							</td>
-							<!-- Delete -->
-							<td>
-								<form action="{{route('admin.detail.delete',$data->id)}}" method="POST" role="form">
-									{{csrf_field()}}
-									<input type="hidden" name="method" value="DELETE">
-									<input type="hidden" name="id" value="{{$data->id}}">
-									<button class="btn btn-link" id="delete" type="submit" onclick="return confirmdelete('Are you sure you want to delete -{{$data->tittle}}- ?')"><span class="glyphicon glyphicon-trash">&nbsp</span>Delete</button>
-								</form>
-							</td>
-						</tr>
-						@endforeach
-					</tbody>
-				</table>
-			</div>
-
 		</div>
-
-		<a href="{{url('admin/detail/add')}}"><button class="btn btn-info pull-right">Add Detail</button></a>
+			<a href="{{url('admin/detail/add')}}"><button class="btn btn-info pull-right">Add Detail</button></a>
 	</div>
 
 @stop
